885b083c94016b09158d78e7c50df5600c617ee3
natedana <natepdana@gmail.com>
2017-10-21 09:11:18 -0700

    simplify

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/image',function(req,res){
-  const imageContent = res.body.base64;
-  processor(imageContent).then(a=>{
-    console.log(a);
-    res.json(a)
-  }).catch(err=>{
-    console.log(err);
-  })
+  console.log(req.body.base64);
+  // const imageContent = res.body.base64;
+  // processor(imageContent).then(a=>{
+  //   console.log(a);
+  //   res.json(a)
+  // }).catch(err=>{
+  //   console.log(err);
+  // })
+  res.send({success:true})
 })
 module.exports = router;
