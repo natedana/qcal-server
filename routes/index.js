db9df4470226517e4cd446c4fbcbfacb4da68062
natedana <natepdana@gmail.com>
2017-10-21 09:27:42 -0700

    finalize

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/image',function(req,res){
-  console.log(req.body.base64);
-  // const imageContent = res.body.base64;
-  // processor(imageContent).then(a=>{
-  //   console.log(a);
-  //   res.json(a)
-  // }).catch(err=>{
-  //   console.log(err);
-  // })
-  res.send({success:true})
+  console.log(req.body);
+  const imageContent = req.body;
+  processor(imageContent).then(a=>{
+    console.log(a);
+    res.json(a)
+  }).catch(err=>{
+    console.log(err);
+  })
 })
 module.exports = router;
